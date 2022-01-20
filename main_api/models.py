e9cd271eb3b3b7e0b32359bb58237746f08a18bc
Temus <temus@wir.pl>
2022-01-20 17:08:24 +0100

    update db models

--- main_api/models.py
+++ main_api/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField("Author")
-    published_date = models.IntegerField()
+    published_date = models.PositiveSmallIntegerField(
+        default=1000,
+        validators=[
+            MaxValueValidator(2020),
+            MinValueValidator(1000)
+        ]
+    )
     categories = models.ManyToManyField("Category")
-    average_raging = models.IntegerField()
-    ratings_count = models.IntegerField()
+    average_raging = models.PositiveSmallIntegerField(default=0)
+    ratings_count = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self) -> str:
         return self.title
