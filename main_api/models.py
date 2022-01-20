0078d9ecf5513205121e7acc37afcd60bfeaca54
Temus <temus@wir.pl>
2022-01-20 16:53:23 +0100

    add models and serializers

--- main_api/models.py
+++ main_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField("Author")
+    published_date = models.IntegerField()
+    categories = models.ManyToManyField("Category")
+    average_raging = models.IntegerField()
+    ratings_count = models.IntegerField()
+
+    class Meta:
+        ordering = ['title', 'authors', 'published_date', 'categories', 'average_rating', 'ratings_count', 'thumbnail']
+
+    def __str__(self) -> str:
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
