b249d6a7526b327d9e1387b28d1c8785f2fbf3ea
Temus <temus@wir.pl>
2022-01-20 16:55:30 +0100

    update admin db

--- main_api/admin.py
+++ main_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main_api.models import Book, Author, Category
 
 # Register your models here.
+
+admin.register(Book)
+admin.register(Author)
+admin.register(Category)
