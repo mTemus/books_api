27e819022b2cd87bdb516a2aaa2877507e2c83cf
Temus <temus@wir.pl>
2022-01-22 11:51:45 +0100

    update loading data from env

--- booksApi/settings.py
+++ booksApi/settings.py
@@ -13,6 +13,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
