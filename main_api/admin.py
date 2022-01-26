e168c798531e7fd6aae3db362a20026bd2005bf8
Temus <temus@wir.pl>
2022-01-26 17:14:09 +0100

    add models for m2m relationship

--- main_api/admin.py
+++ main_api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from main_api.models import Book, Author, Category
+from main_api.models import Book, Author, Category, BookAuthor, BookCategory
 
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(BookAuthor)
+admin.site.register(BookCategory)
--- main_api/models.py
+++ main_api/models.py
@@ -2,32 +2,40 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField("Author")
-    published_date = models.PositiveSmallIntegerField(
-        default=1000,
-        validators=[
-            MaxValueValidator(2020),
-            MinValueValidator(1000)
-        ]
-    )
-    categories = models.ManyToManyField("Category")
-    average_rating = models.PositiveSmallIntegerField(default=0)
-    ratings_count = models.PositiveSmallIntegerField(default=0)
-    thumbnail = models.CharField(max_length=300, default="")
-
-    def __str__(self) -> str:
-        return self.title
-
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=1000)
+    authors = models.ManyToManyField(Author, through='BookAuthor')
+    published_date = models.PositiveSmallIntegerField(
+        default=1000,
+        validators=[
+            MaxValueValidator(2022),
+            MinValueValidator(1000)
+        ]
+    )
+    categories = models.ManyToManyField(Category, through='BookCategory')
+    average_rating = models.PositiveSmallIntegerField(default=0)
+    ratings_count = models.PositiveSmallIntegerField(default=0)
+    thumbnail = models.CharField(max_length=300, default="")
+
+class BookAuthor(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+class BookCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
