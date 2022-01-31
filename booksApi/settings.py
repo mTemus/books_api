ae6250c451b30ddf8141d9a8e884e91b909f06ce
Temus <temus@wir.pl>
2022-01-31 12:02:08 +0100

    update database connection

--- booksApi/settings.py
+++ booksApi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 # Filtering
 REST_FRAMEWORK = {
@@ -91,11 +91,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'books_api', 
-        'USER': 'postgres', 
-        'PASSWORD': os.getenv('POSTGRESS_PASS'),
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_NAME'), 
+        'USER': os.getenv('POSTGRES_USER'), 
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'), 
+        'HOST': os.getenv('POSTGRES_HOST'),  
+        'PORT': os.getenv('POSTGRES_PORT'), 
     }
 }
 
